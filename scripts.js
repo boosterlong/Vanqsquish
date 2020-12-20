0975460af66a3b33926095ea5cd1b594ef7f61d4
Blake Chartrand <chartrandblake@hotmail.com>
2020-12-20 13:27:37 -0800

    Fixed the healthpool

--- scripts.js
+++ scripts.js
@@ -1,20 +1,15 @@
 
-
+//Put the text input next to the class and show the box with the enemy
 function createHero() {
     var input = document.getElementById('userinput');
     var input2 = document.querySelector('input[name="option"]:checked');
     let hero = (input.value + ' the ' + input2.value);
+//Saves hero title and name
     localStorage.setItem('heroName', hero)
     alert(hero + " has been born!")
-
-
-//This generates the enemy health pool.
-    let health = Math.floor(Math.random() * 10 + 1);
-    localStorage.setItem('enemyHP', health)
-
 } 
 
-
+//This is the function to show the enemy, used in createHero()
 function toggleText() {
     var text = document.getElementById("adventure");
     if (text.style.display === "none") {
@@ -22,11 +17,11 @@ function toggleText() {
     }
   }
 
+  //Attack the bug by generating a random damage number and comparing it to enemy health
   function attackBug() {
     let damage = Math.floor(Math.random() * 10 + 1);
     alert((localStorage.getItem('heroName')) + " fights the enemy and does " + damage + " damage!");
-    localStorage.setItem('attackResult', damage)
-  if (localStorage.getItem('enemyHP') <= damage) {
+  if ((localStorage.getItem('enemyHP')) < damage) {
     resolveBattle();}
     else {
       alert('The bug remains alive and regenerates its lost health!')
@@ -35,7 +30,6 @@ function toggleText() {
 
 function resolveBattle() {
     window.location.href = "./victory.html"
-
 }
 
 function backToMain() {
@@ -47,5 +41,8 @@ function recallName() {
 }
 
 function enemyPool() {
+  //This generates the enemy health pool and posts it.
+  const health = Math.floor(Math.random() * 10 + 1);
+  localStorage.setItem('enemyHP', health)
   document.write(localStorage.getItem('enemyHP'))
 }
